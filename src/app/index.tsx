c9590d3b3796e5671b421804bc1c9c1cf5b648d3
MacBookPro <macbookpro@Gold-MacBookPros-Pro.local>
2024-11-25 19:56:25 +0100

    Include Asset

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+import React from "react";
+
+const index = () => {
+  return (
+    <View>
+      <Text>Your name hold</Text>
+    </View>
+  );
+};
+
+export default index;
